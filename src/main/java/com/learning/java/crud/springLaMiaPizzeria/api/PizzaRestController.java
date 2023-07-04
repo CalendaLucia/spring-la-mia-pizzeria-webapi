7e0b8d7d08c63570f5af3b4c9054762b0fb9aafa
CalendaLucia <calendalucia4@gmail.com>
2023-07-04 14:57:34 +0200

    leggere i dettagli di una singola pizza fatto

--- src/main/java/com/learning/java/crud/springLaMiaPizzeria/api/PizzaRestController.java
+++ src/main/java/com/learning/java/crud/springLaMiaPizzeria/api/PizzaRestController.java
@@ -3,19 +3,24 @@ package com.learning.java.crud.springLaMiaPizzeria.api;
 import com.learning.java.crud.springLaMiaPizzeria.model.Pizza;
 import com.learning.java.crud.springLaMiaPizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/papas")
+@RequestMapping("api/v1/pizzas")
 public class PizzaRestController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
+    // ottenere la lista di pizze (dobbiamo anche poterle filtrare per titolo)
     @GetMapping
-    public List<Pizza> index(@RequestParam(name = "keyword", required = false) String search) {
+    public List<Pizza> getPizzas(@RequestParam(name = "keyword", required = false) String search) {
 
         List<Pizza> pizzas;
         if (search == null || search.isBlank()) {
@@ -26,6 +31,19 @@ public class PizzaRestController {
             pizzas = pizzaRepository.findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(search, search);
         }
         return pizzas;
+    }
+
+    //leggere i dettagli di una singola pizza
+
+    @GetMapping("/{pizzaId}")
+    public ResponseEntity<Pizza> getPizza(@PathVariable Integer pizzaId) {
+        Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get());
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
