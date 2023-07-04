554dde34684bfabcb618735a3a041074ba552473
CalendaLucia <calendalucia4@gmail.com>
2023-07-04 16:28:43 +0200

    modificare una pizza esistente fatto cancellare una pizza fatto

--- src/main/java/com/learning/java/crud/springLaMiaPizzeria/api/PizzaRestController.java
+++ src/main/java/com/learning/java/crud/springLaMiaPizzeria/api/PizzaRestController.java
@@ -2,13 +2,16 @@ package com.learning.java.crud.springLaMiaPizzeria.api;
 
 import com.learning.java.crud.springLaMiaPizzeria.model.Ingredient;
 import com.learning.java.crud.springLaMiaPizzeria.model.Pizza;
+import com.learning.java.crud.springLaMiaPizzeria.repository.IngredientRepository;
 import com.learning.java.crud.springLaMiaPizzeria.repository.PizzaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,9 @@ import java.util.Optional;
 public class PizzaRestController {
     @Autowired
     private PizzaRepository pizzaRepository;
+
+    @Autowired
+    IngredientRepository ingredientRepository;
 
     // ottenere la lista di pizze (dobbiamo anche poterle filtrare per titolo)
     @GetMapping
@@ -36,9 +42,10 @@ public class PizzaRestController {
 
     //leggere i dettagli di una singola pizza
 
-    @GetMapping("/{pizzaId}")
+    @GetMapping("{pizzaId}")
     public ResponseEntity<Pizza> getPizza(@PathVariable Integer pizzaId) {
         Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+
         if (result.isPresent()) {
             Pizza pizza = result.get();
             List<Ingredient> ingredients = pizza.getIngredients();
@@ -50,4 +57,35 @@ public class PizzaRestController {
         }
     }
 
+    //creare una nuova pizza
+    @PostMapping
+    public Pizza create(@Valid @RequestBody Pizza pizza) {
+        Pizza newPizza = new Pizza();
+        newPizza.setCreatedAt(LocalDateTime.now());
+        newPizza.setName(pizza.getName());
+        newPizza.setPrice(pizza.getPrice());
+        newPizza.setDescription(pizza.getDescription());
+        newPizza.setIngredients(pizza.getIngredients());
+        newPizza.setPhoto(pizza.getPhoto());
+
+
+        return pizzaRepository.save(newPizza);
+    }
+
+    // modificare una pizza esistente
+
+    @PutMapping("/{pizzaId}")
+    public Pizza update(
+            @Valid
+            @RequestBody Pizza pizza,
+            @PathVariable Integer pizzaId) {
+        pizza.setId(pizzaId);
+        return pizzaRepository.save(pizza);
+
+    }
+
+    @DeleteMapping("/{pizzaId}")
+    public void delete(@PathVariable Integer pizzaId) {
+        pizzaRepository.deleteById(pizzaId);
+    }
 }
